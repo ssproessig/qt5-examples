1be1b50e910715fcb8107cb51b24a75fe74ab07a
Sören Sprößig <ssproessig@googlemail.com>
2020-07-01 16:11:44 +0200

    adding TLSv1.3 secured Client and Server

---
+++ SslUsage/SecureEchoService/Client.cpp
@@ -0,0 +1,75 @@
+#include "Shared.h"
+
+#include <QCommandLineParser>
+#include <QCoreApplication>
+#include <QSslCipher>
+#include <QTcpSocket>
+
+
+int main(int argc, char** argv)
+{
+    QCoreApplication a(argc, argv);
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("RawEchoServer application");
+    (void) parser.addHelpOption();
+
+    (void) parser.addOption({{"p", "port"}, "port to connect to", "port", "9876"});
+    (void) parser.addOption( //
+            {{"i", "host"}, "host to connect to", "host", "127.0.0.1"});
+    (void) parser.addOption( //
+            {{"c", "cert"}, "Client certificate to use", "cert", ":/Certificate"});
+    (void) parser.addOption( //
+            {{"k", "key"}, "Client key to use", "key", ":/Key"});
+    (void) parser.addOption( //
+            {{"w", "pwd"}, "Key password to use", "pwd", "client_01"});
+
+    parser.process(QCoreApplication::arguments());
+
+    auto const host = parser.value("host");
+    auto const port = parser.value("port").toUShort();
+
+    auto const certFileName = parser.value("cert");
+    auto const keyFileName = parser.value("key");
+    auto const keyPwd = parser.value("pwd");
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // actual secure socket communication part - changes to
+    // 1. use QSslSocket instead of QTcpSocket
+    // 2. connect to QSslSocket::sslErrors signal to find out why connection failed
+    // 3. a) use connectToHostEncrypted() and waitForEncrypted(<timeout_in_ms>)
+    //    b) OR use the signal encrypted() if you want to continue already and wait
+    //       for secure connection asynchronously -QSslSocket has some internal buffering,
+    //       you can even send already...
+    QSslSocket s;
+    setupSslConfigurationFor(s, loadKey(keyFileName, "client_01"), loadCert(certFileName));
+
+    QObject::connect(&s, QOverload<SslErrs>::of(&QSslSocket::sslErrors), [&s](SslErrs errors) {
+        dumpSslErrors(errors, s);
+    });
+
+    QObject::connect(&s, &QTcpSocket::readyRead, [&s, &a]() {
+        auto const data = s.readAll();
+        qDebug() << "received: " << data;
+        a.quit();
+    });
+
+    qInfo() << "connecting to" << host << ":" << port << "...";
+    s.connectToHostEncrypted(host, port);
+    if (s.waitForEncrypted(5000))
+    {
+        qInfo() << "we have secure communication";
+        qDebug() << "session cipher" << s.sessionCipher();
+
+        s.write("hello from secure client!");
+    }
+    else
+    {
+        qCritical() << "unable to connect securely";
+        return 1;
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    return a.exec();
+}
---
+++ SslUsage/SecureEchoService/Shared.h
@@ -0,0 +1,96 @@
+#include <QDebug>
+#include <QFile>
+#include <QSslCertificate>
+#include <QSslConfiguration>
+#include <QSslKey>
+#include <QSslSocket>
+#include <QString>
+
+#include <algorithm>
+#include <stdexcept>
+
+
+QByteArray readFromQrc(QString const& aFileName)
+{
+    QFile f(aFileName);
+    if (f.open(QIODevice::ReadOnly))
+    {
+        return f.readAll();
+    }
+
+    qCritical() << QString("unable to read resource: %1").arg(aFileName);
+    return {};
+}
+
+
+QSslKey loadKey(QString const& aFileName, QByteArray const& pwd)
+{
+    auto const key = QSslKey(readFromQrc(aFileName), QSsl::Rsa, QSsl::Pem, QSsl::PrivateKey, pwd);
+    qDebug() << "error loading key?" << key.isNull();
+    qInfo() << "Loaded key: " << key;
+
+    return key;
+}
+
+
+QSslCertificate loadCert(QString const& aFileName)
+{
+    auto const cert = QSslCertificate(readFromQrc(aFileName), QSsl::Pem);
+    qDebug() << "error loading cert?" << cert.isNull();
+    qInfo() << "Loaded certificate: " << cert;
+
+    return cert;
+}
+
+
+void setupSslConfigurationFor(QSslSocket& s, QSslKey const& key, QSslCertificate const& cert)
+{
+    // alter the sockets existing configuration
+    auto sslConfig = s.sslConfiguration();
+    {
+        // enforce TLS v1.3
+        sslConfig.setProtocol(QSsl::TlsV1_3);
+
+        // don't use a CA chain - only our own Certificate Authority is allowed
+        sslConfig.setCaCertificates({QSslCertificate(readFromQrc(":/CA"), QSsl::Pem)});
+
+        // load the passed certificate with the given key
+        sslConfig.setPrivateKey(key);
+        sslConfig.setLocalCertificate(cert);
+
+        // verify the connection peer's certificate - fail connection if it fails
+        // note: QSslSocket::QueryPeer will only warn if the peer verification failed - discouraged
+        sslConfig.setPeerVerifyMode(QSslSocket::VerifyPeer);
+    }
+    s.setSslConfiguration(sslConfig);
+}
+
+
+void dumpCert(QSslCertificate const& cert)
+{
+    qDebug() << "---";
+    qDebug() << "digest: " << cert.digest().toHex();
+    qDebug() << "serial: " << cert.serialNumber().toHex();
+    qDebug() << "---subject";
+    qDebug() << "CN: " << cert.subjectInfo(QSslCertificate::CommonName).join(' ');
+    qDebug() << "ORG:" << cert.subjectInfo(QSslCertificate::Organization).join(' ');
+    qDebug() << "---issuer";
+    qDebug() << "CN: " << cert.issuerInfo(QSslCertificate::CommonName);
+    qDebug() << "ORG:" << cert.issuerInfo(QSslCertificate::Organization);
+    qDebug() << "";
+}
+
+
+using SslErrs = QList<QSslError> const&;
+void dumpSslErrors(SslErrs errors, QSslSocket const& forSocket)
+{
+    qDebug() << "SSL errors:";
+    for (auto const& error : errors)
+    {
+        qCritical() << error.errorString();
+    }
+
+    qDebug() << "peer certificate chain:";
+    auto const& peerCertificateChain = forSocket.peerCertificateChain();
+    std::for_each(peerCertificateChain.cbegin(), peerCertificateChain.cend(), dumpCert);
+}
